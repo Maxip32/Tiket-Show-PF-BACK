097e65c51305b41581c3d19d27d8574b98f58168
Maxip32 <piolattoemanuel@gmail.com>
2023-08-08 01:35:16 -0300

    de true a false el back

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const {
 
 const port = process.env.PORT || 3001;
 // Syncing all the models at once
-conn.sync({ force: true}).then(async () => {
+conn.sync({ force: false }).then(async () => {
   loadUsers();
   loadEvents();
   loadArtists();
